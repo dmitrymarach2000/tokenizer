4effdf031f2e40e0cff310176f6c51ca4b8b15db
U-PI\student <student@PC8.pi.iit.mirea.ru>
2019-11-22 17:32:58 +0300

    >nice

--- main.c
+++ main.c
@@ -3,61 +3,63 @@
 #include <string.h>
 
 struct Token{
-int pos;
-char* str;
+	int pos;
+	char* str;
 };
 
 int main(){
 
-int ch;
-int i = 0;
-char* buf_str;
-int buf_pos;
-struct Token* tokens = (struct Token*)malloc(0);
-int token_n = 0;
-char new_wrd = 1;
-char last_word = 1;
-while((ch = getchar()) != '\n'){
- i++;
- if(ch != '\n' &&
- ch != '\t' &&
- ch != '\r' &&
- ch != ' ' ){
- if(new_wrd){
- last_word = 1;
- new_wrd = 0;
- buf_str = (char*)malloc(1 * sizeof(char));
- buf_pos = i;
- } else{
- buf_str = (char*)realloc(buf_str, ((i - buf_pos) + 1) * sizeof(char));
- }
- buf_str[i - buf_pos] = ch;}
- else { if(!new_wrd){
-	 printf("word");
- new_wrd = 1;
- buf_str = (char*)realloc(buf_str, ((i - buf_pos) + 1) * sizeof(char));
- buf_str[i - buf_pos] = '\0';
- if(1){//Вставить проверку токена
- tokens = (struct Token*)realloc(tokens, ++token_n);
- tokens[token_n - 1].pos = buf_pos;
- tokens[token_n - 1].str = (char*)malloc(((i - buf_pos) + 1) * sizeof(char));
- strcpy(tokens[token_n - 1].str, buf_str);
- }
- } else { last_word = 0; }
-}
-} if(last_word){
- buf_str = (char*)realloc(buf_str, ((i - buf_pos) + 1) * sizeof(char));
- buf_str[i - buf_pos] = '\0';
- if(1){//Вставить проверку токена
- tokens = (struct Token*)realloc(tokens, ++token_n);
- tokens[token_n - 1].pos = buf_pos;
- tokens[token_n - 1].str = (char*)malloc(((i - buf_pos) + 1) * sizeof(char));
- strcpy(tokens[token_n - 1].str, buf_str);
- }
+	int ch;
+	int i = 0;
+	char* buf_str;
+	int buf_pos;
+	struct Token* tokens = (struct Token*)malloc(0);
+	int token_n = 0;
+	char new_wrd = 1;
+	char last_word = 1;
+	while((ch = getchar()) != '\n'){
+		i++;
+		if(
+			ch != '\n' &&
+			ch != '\t' &&
+			ch != '\r' &&
+			ch != ' ' ){
+			
+			if(new_wrd){
+				last_word = 1;
+				new_wrd = 0;
+				buf_str = (char*)malloc(1 * sizeof(char));
+				buf_pos = i;
+			} else{
+				buf_str = (char*)realloc(buf_str, ((i - buf_pos) + 1) * sizeof(char));
+			}
+			buf_str[i - buf_pos] = ch;}
+			else { if(!new_wrd){
+				printf("word");
+				new_wrd = 1;
+				buf_str = (char*)realloc(buf_str, ((i - buf_pos) + 1) * sizeof(char));
+				buf_str[i - buf_pos] = '\0';
+				if(1){//Вставить проверку токена
+					tokens = (struct Token*)realloc(tokens, ++token_n);
+					tokens[token_n - 1].pos = buf_pos;
+					tokens[token_n - 1].str = (char*)malloc(((i - buf_pos) + 1) * sizeof(char));
+					strcpy(tokens[token_n - 1].str, buf_str);
+				}
+				} else { last_word = 0; }
+				}
+				} if(last_word){
+				buf_str = (char*)realloc(buf_str, ((i - buf_pos) + 1) * sizeof(char));
+				buf_str[i - buf_pos] = '\0';
+				if(1){//Вставить проверку токена
+					tokens = (struct Token*)realloc(tokens, ++token_n);
+					tokens[token_n - 1].pos = buf_pos;
+					tokens[token_n - 1].str = (char*)malloc(((i - buf_pos) + 1) * sizeof(char));
+					strcpy(tokens[token_n - 1].str, buf_str);
+				}
 }
 
-printf("\n\n");
-for(int i = 0; i < token_n; ++i){
-printf("%d\t%s\n", tokens[i].pos, tokens[i].str);
-}
+	printf("\n\n");
+	for(int i = 0; i < token_n; ++i){
+		printf("%d\t%s\n", tokens[i].pos, tokens[i].str);
+	}
 }
