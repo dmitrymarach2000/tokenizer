4b52ff9e3dfd6a24af50396b9386cd7b630e650a
U-PI\student <student@PC8.pi.iit.mirea.ru>
2019-11-22 18:05:20 +0300

    1 - debug symbols

--- main.c
+++ main.c
@@ -18,6 +18,7 @@ int main(){
 	char new_wrd = 1;
 	char last_word = 1;
 	while((ch = getchar()) != '\n'){
+		printf("\n~'%c'", ch);
 		i++;
 		if(
 			ch != '\n' &&
@@ -26,19 +27,21 @@ int main(){
 			ch != ' ' ){
 			
 			if(new_wrd){
+				printf("^\n");
 				last_word = 1;
 				new_wrd = 0;
 				buf_str = (char*)malloc(1 * sizeof(char));
 				buf_pos = i;
 			} else{
+				printf("*\n");
 				buf_str = (char*)realloc(buf_str, ((i - buf_pos) + 1) * sizeof(char));
 			}
 			buf_str[i - buf_pos] = ch;}
 			else { if(!new_wrd){
-				printf("word");
 				new_wrd = 1;
 				buf_str = (char*)realloc(buf_str, ((i - buf_pos) + 1) * sizeof(char));
 				buf_str[i - buf_pos] = '\0';
+				printf("word'%s'", buf_str);
 				if(1){//Вставить проверку токена
 					tokens = (struct Token*)realloc(tokens, ++token_n);
 					tokens[token_n - 1].pos = buf_pos;
@@ -48,6 +51,7 @@ int main(){
 				} else { last_word = 0; }
 				}
 				} if(last_word){
+				printf("\n=\n");
 				buf_str = (char*)realloc(buf_str, ((i - buf_pos) + 1) * sizeof(char));
 				buf_str[i - buf_pos] = '\0';
 				if(1){//Вставить проверку токена
